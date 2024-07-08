e8212b605f9322d3f2357242cb0d445bcadd6332
tejaswi1508 <inboxofteju@gmail.com>
2024-07-08 23:13:30 +0530

    LL from Array

---
+++ cpp/LL1.cpp
@@ -0,0 +1,14 @@
+lass Solution {
+  public:
+    Node* constructLL(vector<int>& arr) {
+        // code here
+        Node* head = new Node(arr[0]);
+        Node* mover = head;
+        for(int i = 1; i < arr.size();i++){
+            Node* temp = new Node(arr[i]);
+            mover->next = temp;
+            mover = temp;
+        }
+        return head;
+    }
+};
