ce2d23880af797d0a61fbb685210a5063d8364e5
tejaswi1508 <inboxofteju@gmail.com>
2024-07-11 21:26:59 +0530

    To detect loop in LL

---
+++ cpp/LL7.cpp
@@ -0,0 +1,15 @@
+/*To detect loop in LL*/
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(fast==slow) return true;
+        }
+        return false;
+    }
+};
