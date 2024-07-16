100196fcf1bcb0d33ed565fc75b7f2f49c3ae39e
tejaswi1508 <inboxofteju@gmail.com>
2024-07-16 08:41:39 +0530

    Ram question

---
+++ cpp/CF_RAM.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    vector<pair<int,int>> inp(t);
+    for (int i = 0; i < t; ++i) {
+        cin >> inp[i].first;
+        cin >> inp[i].second;
+    }
+
+    for(auto x : inp){
+        int res = (x.first-1)*x.second + 1;
+        cout << res << endl;
+    }
+    return 0;
+}
