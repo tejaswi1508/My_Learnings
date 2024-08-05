0e4243318639ad45cd48bf61d6618304d91ba30d
tejaswi1508 <inboxofteju@gmail.com>
2024-08-05 23:25:47 +0530

    Number of Good Pairs

---
+++ DailyProb10.cpp
@@ -0,0 +1,17 @@
+/*Number of Good Pairs*/
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        map<int,int>mp;
+       int ans=0;
+       for (int i=0;i<nums.size();i++)
+       {
+           if (mp.find(nums[i])!=mp.end())
+           {
+               ans+=mp[nums[i]];
+           }
+           mp[nums[i]]++;
+       }
+        return ans;
+    }
+};
