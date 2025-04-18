367b8ef48bbb2706539817bb93121be78915eacf
tejaswi1508 <inboxofteju@gmail.com>
2025-04-18 18:00:47 +0530

    20. Valid Parentheses

---
+++ cpp/ValidParentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    public:
+        bool isValid(string s) {
+            stack<int> st;
+            int n = s.size();
+            for(int i=0;i<n;i++){
+                if(s[i]=='('||s[i]=='['||s[i]=='{') st.push(s[i]);
+                else {
+                    if(st.empty()||
+                        s[i]==')'&&st.top()!='('||
+                        s[i]==']'&&st.top()!='['||
+                        s[i]=='}'&&st.top()!='{'){
+                            return false;
+                        }
+                        st.pop();
+                            
+                }
+            }
+            return st.empty();
+        }
+    };
