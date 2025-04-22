0e95461b5086d545cace392ccb9abcd2cc3c3036
tejaswi1508 <inboxofteju@gmail.com>
2025-04-22 22:11:06 +0530

    optimized

--- cpp/Arrays7.cpp
+++ cpp/Arrays7.cpp
@@ -3,9 +3,10 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
         long long maxi = LONG_MIN; 
         long long sum = 0;
-        int n = nums.size();
+        
         for (int i = 0; i < n; i++) {
 
             sum += nums[i];
