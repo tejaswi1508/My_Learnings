d833862b445697059658a5a0cee9c97de81c37b3
tejaswi1508 <inboxofteju@gmail.com>
2025-04-24 22:01:29 +0530

    .

--- cpp/Arrays13.cpp
+++ cpp/Arrays13.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int n = nums.size(); 
-        
+        int n = nums.size();
         int ans = 0; 
         
         for(int i = 0; i < n; i++) 
