04a4c57755a4cce62102711baadaac202e12c98b
tejaswi1508 <inboxofteju@gmail.com>
2025-05-01 22:37:15 +0530

    MinStack

--- cpp/MinStack.cpp
+++ cpp/MinStack.cpp
@@ -1,4 +1,13 @@
 
+/**
+     * Your MinStack object will be instantiated and called as such:
+     * MinStack* obj = new MinStack();
+     * obj->push(val);
+     * obj->pop();
+     * int param_3 = obj->top();
+     * int param_4 = obj->getMin();
+     */
+    
 class MinStack {
     public:
         vector<int> st;
@@ -29,11 +38,4 @@ class MinStack {
         }
     };
     
-    /**
-     * Your MinStack object will be instantiated and called as such:
-     * MinStack* obj = new MinStack();
-     * obj->push(val);
-     * obj->pop();
-     * int param_3 = obj->top();
-     * int param_4 = obj->getMin();
-     */
+    
