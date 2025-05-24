6aaf5de08f3d48f3842b9fff70729b9be62c5d7f
tejaswi1508 <inboxofteju@gmail.com>
2025-05-24 20:06:45 +0530

    Inserting at head of Linked List

--- cpp/LinkedList.cpp
+++ cpp/LinkedList.cpp
@@ -1,23 +1,12 @@
 //Insert at head
-void insertAtHead(Node* &head, int val) {
-    Node* newNode = new Node(val);
-    newNode->next = head;
+void insertAtHead(Node* &head,int val){
+    Node* newNode = new Node(val,head);
     head = newNode;
 }
 
 //Insert at tail
-void insertAtTail(Node* &head, int val) {
-    Node* newNode = new Node(val);
-    if (head == NULL) {
-        head = newNode;
-        return;
-    }
-    Node* temp = head;
-    while (temp->next != NULL) {
-        temp = temp->next;
-    }
-    temp->next = newNode;
-}
+
+
 class Node {
     public:
         int data;         // Data stored in the node
