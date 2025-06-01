6b99dc110aaf7ef761c3c3abaad96c0fbf00064f
tejaswi1508 <inboxofteju@gmail.com>
2025-06-01 22:34:39 +0530

    ..

--- cpp/LinkedList.cpp
+++ cpp/LinkedList.cpp
@@ -31,7 +31,7 @@ class Node {
     public:
         int data;         // Data stored in the node
         Node* next;       // Pointer to the next node in the linked list
-        // Constructors
+        
         Node(int data1, Node* next1) {
             data = data1;
             next = next1;
