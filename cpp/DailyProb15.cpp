99cd72deec085ad19967a2766375051e8c32f3f2
tejaswi1508 <inboxofteju@gmail.com>
2025-06-12 22:40:07 +0530

    2798 update

--- cpp/DailyProb15.cpp
+++ cpp/DailyProb15.cpp
@@ -11,3 +11,14 @@ public:
         return cnt;
     }
 };
+
+//Alternate solution
+
+class Solution {
+public:
+    int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
+        return count_if(hours.begin(), hours.end(), [&](int h) {
+            return h >= target;
+        });
+    }
+};
